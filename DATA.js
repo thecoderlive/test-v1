e0da6c1fcec9430c2358f525959e9e8f9c004c17
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-13 18:36:39 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "7a7cca60-bfc8-44bf-b50f-6bec8be79cec",
+    "home_feed": [
+        {
+            "id": "ed0a6bdb-ea98-4c62-9113-e7cdf4262ae4",
+            "artist_name": "middle",
+            "home_carousel": [
+                {
+                    "id": "d2995a37-43e0-41a3-92cd-b50da6d9736c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "7aa7c59d-d2f6-4344-bcd0-dc693d03a3ee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                },
+                {
+                    "id": "004b3d0e-9688-44ea-a946-1289199cea9d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "57324b26-bcd5-43a1-ae16-69a58c5a85ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "6f3fa6cc-9da2-43c4-9c17-21e5690c98d5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/79.png"
+                }
+            ],
+            "art_caption": "sister",
+            "art_description": "Require operation director often. May today control growth."
+        },
+        {
+            "id": "0c2a8399-4ca2-4257-8ac0-46d59436e94f",
+            "artist_name": "international",
+            "home_carousel": [
+                {
+                    "id": "25b2d131-a0e5-4590-b582-92f21274f099",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                },
+                {
+                    "id": "c58b10ef-3973-452d-97f0-61710d375c03",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "edcf300a-ef0c-4b1e-ba09-4c579b1e5d0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "499b4365-01b8-4549-9395-8dd7a782061a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png"
+                },
+                {
+                    "id": "5be44e11-ef5a-46e2-b235-54cdf8cdcb36",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                }
+            ],
+            "art_caption": "nor",
+            "art_description": "Either that necessary. Reflect result throw partner each economy."
+        },
+        {
+            "id": "710e23e8-596c-479a-a531-a121b9800a13",
+            "artist_name": "little",
+            "home_carousel": [
+                {
+                    "id": "fb0a2644-68d1-410e-96fb-89d1ccfc4def",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "1210b70d-0022-493b-997a-4c51d84b7b88",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "20853a59-50af-4fda-857c-852d22319314",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/347.png"
+                },
+                {
+                    "id": "b365a50e-6ed5-40b2-91dd-9fb3bf8c3f56",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                },
+                {
+                    "id": "4b081279-3c7e-4f51-815c-6cd1c000558a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                }
+            ],
+            "art_caption": "anything",
+            "art_description": "Difficult rise fire produce vote modern. Common structure member direction financial send participant. Around result will charge future force yourself cold."
+        },
+        {
+            "id": "0fcda714-557a-489c-99bb-3cff60348566",
+            "artist_name": "speak",
+            "home_carousel": [
+                {
+                    "id": "a2f762fa-12d8-4f53-8294-d7f4a0cf1e52",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png"
+                },
+                {
+                    "id": "4aa55827-adc0-450b-8722-309d768975d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "891d0849-226f-45dd-ab52-db21ce90d391",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "7b83e18f-fc44-431a-883e-a728c2c08a56",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                },
+                {
+                    "id": "1877ab47-2ca6-4f91-beb2-926530215573",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                }
+            ],
+            "art_caption": "sense",
+            "art_description": "Song high moment."
+        },
+        {
+            "id": "6abf2445-39f3-4ec0-b695-0dae16dc38ad",
+            "artist_name": "character",
+            "home_carousel": [
+                {
+                    "id": "61f11247-09b9-429a-9758-997fc166749d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "b3765980-4972-4e01-a920-bae7ab7a24d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                },
+                {
+                    "id": "e557e2c9-2a87-4afe-a4dd-9f2fa63c7ff6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                },
+                {
+                    "id": "cfa35db2-2ba5-448a-9ff9-07162071a8e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/341.png"
+                },
+                {
+                    "id": "57042c85-c0c3-47b8-9e23-4894fdfc0caa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/230.png"
+                }
+            ],
+            "art_caption": "yes",
+            "art_description": "Notice national state high the. Arrive play kitchen stage. Health energy total anyone."
+        }
+    ]
+}
